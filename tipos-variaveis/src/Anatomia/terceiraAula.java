2d8b0d022a4e2fabed64bb785fc8b2176a909f5d
fernandovfc <fernandovfc@hotmail.com>
2022-11-02 11:12:09 -0300

    Atualização de repositório de estudos

---
+++ tipos-variaveis/src/Anatomia/terceiraAula.java
@@ -0,0 +1,22 @@
+package Anatomia;
+public class terceiraAula {
+    
+    public static void main (String [] args) {
+        String primeiroNome = "Fernando";
+        String segundoNome = "Freitas";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+
+    
+    }
+
+    private static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+    
+
+    }
+
---
+++ tipos-variaveis/src/TiposeVariaveis/TiposVariaveis.java
@@ -0,0 +1,9 @@
+package TiposeVariaveis;
+public class TiposVariaveis {
+    public static void main(String[] args)  {
+        int numero = 5;
+        numero = - 5;
+    
+        System.out.println(numero);
+    }
+}
